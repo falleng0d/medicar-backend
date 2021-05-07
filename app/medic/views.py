8298c38f7b77bc7cb7d7c2feb17aeab1e7763362
Mateus Junior <juniormateusknd@gmail.com>
2021-05-07 11:39:17 -0300

    Medic require Authenticated

--- app/medic/views.py
+++ app/medic/views.py
@@ -2,14 +2,21 @@ from core.models import Medic
 from medic.serializers import MedicSerializer
 from rest_framework import filters, viewsets
 
+# from django_filters import rest_framework as django_filters
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# from django_filters import rest_framework as filters
+
 class MedicViewSet(viewsets.ModelViewSet):
 	"""
 	API endpoint that allows users to be viewed or edited.
 	"""
 	queryset = Medic.objects.all()
+
 	serializer_class = MedicSerializer
+	authentication_classes = [TokenAuthentication]
+	permission_classes = [IsAuthenticated]
+
 	filter_backends = [filters.SearchFilter]
 	search_fields = ['nome']
 
