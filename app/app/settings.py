b20102165e010748e47fba9eebcfa65b6767c796
Mateus Junior <juniormateusknd@gmail.com>
2021-05-15 16:25:23 -0300

    Add whitenoise

--- app/app/settings.py
+++ app/app/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'corsheaders.middleware.CorsMiddleware',
