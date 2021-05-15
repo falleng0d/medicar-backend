0c5b5b2e73d04dcee5527fc93c8ad187e6de5ac1
Mateus Junior <juniormateusknd@gmail.com>
2021-05-15 16:25:38 -0300

    Improved release.sh

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 cd app
-python manage.py wait_for_db &&
-python manage.py makemigrations &&
-python manage.py migrate &&
-python manage.py setup_test_data &&
+python manage.py wait_for_db
+python manage.py migrate
+python manage.py setup_test_data
 python manage.py createsuperuser --noinput 2&>/dev/null
+exit 1
